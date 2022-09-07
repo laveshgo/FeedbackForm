4ed236e01302a4ca1af3a4972bce704673d4736e
Srishti Toora <srishti@Srishtis-MacBook-Pro.local>
2022-09-07 16:55:59 +0530

    Dashboard complete

--- src/Components/api/DataService.js
+++ src/Components/api/DataService.js
@@ -3,8 +3,16 @@ import axios from 'axios';
 class DataService {
 
     dashboardCreatedByForms (user_id){
-        console.log("created by formss");
-        return axios.get("http://localhost:5050/profilepage")
+        // console.log("created by formss");
+        return axios.get(`http://localhost:5050/profilepage?user_id=${user_id}`)
+    }
+
+    viewResult (form_id){
+        return axios.get (`http://localhost:5050/viewreport?form_id=${form_id}`)
+    }
+
+    viewResponse (response_id){
+        return axios.get(`http://localhost:5050/viewresponse?response_id=${response_id}`)
     }
 
     
--- src/Components/dashboard.jsx
+++ src/Components/dashboard.jsx
@@ -9,61 +9,76 @@ import FillResponse from "./FillResponse";
 import ViewResponses from "./ViewResponses.jsx";
 import { Grid } from "@mui/material";
 import DataService from "./api/DataService";
+import { ThirtyFpsSelect } from "@mui/icons-material";
 
 class Dashbaord extends Component {
   constructor(props) {
     super(props);
     this.state = {
       user_id: 4,
-      formsCreatedByYou: [
-        // { id: 1, description: "Form 1", status: "View" },
-        // { id: 2, description: "Form 2", status: "View" },
-        // { id: 3, description: "Form 3", status: "View" },
-      ],
-      formsForYou: [
-        { id: 1, description: "Form 1", filled: true },
-        { id: 2, description: "Form 2", filled: false },
-        { id: 3, description: "Form 3", filled: true },
-        { id: 4, description: "Form 4", filled: false },
-      ],
+      createdByUser:[],
+      filledByUser:[],
+      notFilledByUser: [],
     };
     this.fillResponse = this.fillResponse.bind(this);
+    this.viewFormCreatedByUser=this.viewFormCreatedByUser.bind(this);
   }
 
-  // const navigate=useNavigate();
 
   fillResponse(param) {
-    console.log(param);
-    // this.navigate('/fill-response')
-
-    // this.props.navigation.navigate('dashboard')
-
-    // console.log("fill response clicked")
-    // this.props.navigate("/fill-response");
+    // console.log(param);
+    
   }
+
 
   componentDidMount(){
-    DataService.dashboardCreatedByForms(this.user_id)
-    .then()
+    this.refreshPage();
+    
+    }
 
+  refreshPage(){
+    DataService.dashboardCreatedByForms(this.state.user_id)
+    .then((response) => {
+     console.log(response.data);
+      this.setState({createdByUser : response.data.createdByUser,
+                     notFilledByUser: response.data.notFilledByUser,
+                    filledByUser: response.data.filledByUser
+      })
+    })
   }
 
-  // viewFormCreatedByYou(){
-  //   DataService.dashboardCreatedByForms(this.state.user_id)
-  //   .then(response => {
+    viewFormCreatedByUser(form_id){
+        // make an api request for view report of single form with the help of form_id
+        // and navigate it to another page
+        console.log(form_id);
 
-  //     console.log(response);
-  //   })
-  // }
+    }
 
-  render() { 
+    viewPendingForm(form_id){
+      // make an api request for filling response with the help of form_id
+        // and navigate it to another page
+        console.log(form_id);
+    }
+
+    // viewResponse(response_id){
+    //   // make an api request for viewing response with the help of form_id
+    //     // and navigate it to another page
+    //     console.log(form_id);
+    // }
+    
+
+    
+
+
+  render()  {     
     return (
       <>
-        <Header />
-
-        <Grid container style={{ paddingTop: "20px", alignItems: "center" }}>
+        <Header /> 
+        
+            <Grid container style={{ paddingTop: "20px", alignItems: "center" }}>
           <Grid item xs={6} style={{ paddingLeft: "50px" }}>
-            <h1>Hello User</h1>
+            <h1>Hello User </h1>
+
           </Grid>
           <Grid item xs={6} style={{ paddingLeft: "50px" }}>
             <Button variant="primary" size="lg">
@@ -78,91 +93,89 @@ class Dashbaord extends Component {
             <h2>Forms Created by You</h2>
             <hr className="hr-css" />
 
+
             <Table striped hover>
-              <thead>
-                <tr>
-                  <th>First Name</th>
-                  <th>Status</th>
-                </tr>
-              </thead>
-              <tbody>
-                {this.state.formsCreatedByYou.map((formByYou) => (
-                  <tr key={formByYou.id}>
-                    <td>{formByYou.description}</td>
-                    <td>
-                      <Button
-                        onClick={this.viewFormCreatedByYou}
-                        variant="primary"
-                        href={"./view-responses?form_id=" + formByYou.id}
-                      >
-                        {formByYou.status}
-                      </Button>{" "}
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </Table>
+             <thead>
+               <tr>
+                 <th>Form Name</th>
+                 <th>Status</th> 
+               </tr>
+             </thead>
+             <tbody>
+               {this.state.createdByUser.map((formByYou) => (
+                 <tr key={formByYou.id}>
+                   <td>{formByYou.name}</td>
+                   <td>
+                     <Button variant="primary" onClick={()=>this.viewFormCreatedByUser(formByYou.id)}>
+                       View
+                     </Button>
+                   </td>
+                 </tr>
+               ))}
+             </tbody>
+           </Table>
+
+          
           </div>
           <div className="float-child">
             <h2>Forms for You</h2>
             <hr className="hr-css" />
 
-            <Table striped>
-              <thead>
-                <tr>
-                  <th>First Name</th>
-                  <th>Status</th>
-                </tr>
-              </thead>
-              <tbody>
-                {this.state.formsForYou.map((formForYou, index) => {
-                  return formForYou.filled ? (
-                    <tr key={formForYou.id}>
-                      <td>{formForYou.description}</td>
-                      <td>
-                        <Button
-                          key={index}
-                          variant="success"
-                          onClick={() => this.fillResponse(formForYou)}
-                          href={
-                            "/your-responses?user_id=" +
-                            formForYou.id +
-                            "&form_id=" +
-                            formForYou.id
-                          }
-                        >
-                          View Response
-                        </Button>{" "}
-                      </td>
-                    </tr>
-                  ) : (
-                    <tr key={formForYou.id}>
-                      <td>{formForYou.description}</td>
-                      <td>
-                        <Button
-                          key={index}
-                          variant="primary"
-                          onClick={() => this.fillResponse(formForYou)}
-                          href={
-                            "/fill-response?user_id=" +
-                            formForYou.id +
-                            "&form_id=" +
-                            formForYou.id
-                          }
-                        >
-                          Fill Response
-                        </Button>{" "}
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </Table>
+            <Table striped hover>
+             <thead>
+               <tr>
+                 <th>Filled Form Name</th>
+                 <th>Status</th> 
+               </tr>
+             </thead>
+             <tbody>
+               {this.state.filledByUser.map((formByYou) => (
+                 <tr key={formByYou.id}>
+                   <td>{formByYou.name}</td>
+                   <td>
+                     <Button variant="primary">
+                       View Response
+                     </Button>
+                   </td>
+                 </tr>
+               ))}
+             </tbody>
+           </Table>
+
+
+            <Table striped hover>
+             <thead>
+               <tr>
+                 <th>Pending Form Name</th>
+                 <th>Status</th> 
+               </tr>
+             </thead>
+             <tbody>
+               {this.state.notFilledByUser.map((formByYou) => (
+                 <tr key={formByYou.id}>
+                   <td>{formByYou.name}</td>
+                   <td>
+                     <Button variant="primary" onClick={()=>this.viewPendingForm(formByYou.id)}>
+                       Fill Response
+                     </Button>
+                   </td>
+                 </tr>
+               ))}
+             </tbody>
+           </Table>
           </div>
         </div>
+
       </>
     );
   }
 }
 
 export default withRouter(Dashbaord);
+
+
+
+
+
+
+
